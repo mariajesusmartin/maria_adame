983df1f5ce751e24061742159f8ba82a45657ab9
mariajesusmartin <mariajesusmartingomez@gmail.com>
2024-07-11 18:58:42 +0200

    Responsive index

--- js/index.js
+++ js/index.js
@@ -12,35 +12,37 @@ aboutImg.addEventListener('mouseout' , function(){
 
 
 
-
-let posicionWeb = 0
-let posicionGraphic = 0
+let posicionWeb = 1
+let posicionGraphic = 1
 
 const proyectsWeb = document.querySelector('.Proyect-ul--web')
 const proyectsGraphic = document.querySelector('.Proyect-ul--graphic')
 
 const desplazarWeb = ()=>{
 
-    posicionWeb++
-    if(posicionWeb > 1){
-        posicionWeb = 0
+    posicionWeb--
+    if(posicionWeb < 0){
+        posicionWeb = 1
     }
     proyectsWeb.style.transform = `translateX(${posicionWeb*(100/2)}%)`
 }
 
+
 const desplazarGraphic = ()=>{
-    posicionGraphic++
-    if(posicionGraphic > 1){
-        posicionGraphic = 0
+    posicionGraphic--
+    if(posicionGraphic < 0){
+        posicionGraphic = 1
     }
     proyectsGraphic.style.transform = `translateX(${posicionGraphic*(100/2)}%)`
 }
+
 
 let automaticoWeb
 let automaticoGraphic
 
 automaticoWeb = setInterval(desplazarWeb, 2000)
 automaticoGraphic = setInterval(desplazarGraphic, 2000)
+
 
 
 
